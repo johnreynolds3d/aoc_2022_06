b4cf5a2333ac6abf72d4d7f3a5d42b5a2f2e3031
John Reynolds <felixinutero@gmail.com>
2022-12-13 12:42:41 +0800

    Done. With a little 'help'...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::collections::HashSet;
 use std::fs::File;
+use std::hash::Hash;
 use std::io;
 use std::io::Read;
+
+// from: https://stackoverflow.com/a/46767732/2017082
+fn has_unique_elements<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Eq + Hash,
+{
+    let mut uniq = HashSet::new();
+    iter.into_iter().all(move |x| uniq.insert(x))
+}
 
 fn main() -> io::Result<()> {
     let mut f = File::open("input.txt")?;
@@ -9,18 +21,15 @@ fn main() -> io::Result<()> {
     f.read_to_end(&mut b)?;
 
     let mut i = 0;
+    let p = 14; // use '4' for Part 1
 
-    while i + 3 < b.len() - 1 {
-        if b[i] == b[i + 1] || b[i] == b[i + 2] || b[i] == b[i + 3] {
-            i += 1;
-        } else if b[i + 1] == b[i + 2] || b[i + 1] == b[i + 3] {
-            i += 2;
-        } else if b[i + 2] == b[i + 3] {
-            i += 3;
-        } else {
-            println!("{}", i + 4);
+    while i + p - 1 < b.len() - 1 {
+        let slice = &b[i..i + p];
+        if has_unique_elements(slice) {
+            println!("{}", i + p);
             return Ok(());
         }
+        i += 1;
     }
     Ok(())
 }
